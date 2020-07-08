661621f852c1cbf3f443801ed9a4dcaa967e282c
rootrsk <ravishankar7050@gmail.com>
2020-07-08 20:04:26 +0530

    idk

--- index.js
+++ index.js
@@ -24,29 +24,30 @@ app.use(cookieParser())
 
 app.use(bodyParser.urlencoded({ extended: true}))
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
-//     // Website you wish to allow to connect
-//     const allowedOrigins = ['https://rootrsk.github.io','http://localhost:3000'];
-//     const origin = req.headers.origin;
-//     if(allowedOrigins.indexOf(origin) > -1){
-//          res.setHeader('Access-Control-Allow-Origin', origin);
-//     }
+    // Website you wish to allow to connect
+    // const allowedOrigins = ['https://rootrsk.github.io','http://localhost:3000'];
+    // const origin = req.headers.origin;
+    // if(allowedOrigins.indexOf(origin) > -1){
+    //      res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
 
-//     // Request methods you wish to allow
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//     // Request headers you wish to allow
-//     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.header('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.header('Access-Control-Allow-Credentials', true);
 
-//     // Pass to next layer of middleware
-//     next();
-// });
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.use(userRouter)
