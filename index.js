0e456ff32238a6f1138cfcc8fd9a5934dcca3dfb
rootrsk <ravishankar7050@gmail.com>
2020-07-08 17:01:42 +0530

    adding some Header

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('./src/database/mongoose')
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://rootrsk.github.io/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
