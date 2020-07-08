e69f6449f928fbe106f89661c0c42eafeb87d7da
rootrsk <ravishankar7050@gmail.com>
2020-07-08 19:59:00 +0530

    idk

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    // res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
