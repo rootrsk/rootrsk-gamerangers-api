51c518988684108c963bdb01746518e81d670531
rootrsk <ravishankar7050@gmail.com>
2020-07-08 17:06:33 +0530

    idk

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://rootrsk.github.io/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
